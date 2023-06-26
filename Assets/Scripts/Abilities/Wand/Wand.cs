42297cdb44c05354bff3cab456f31e439eba1a98
Addison <resiget@gmail.com>
2023-06-25 23:44:38 -0500

    add ability slots, slot wand ability in slot 1

--- Assets/Scripts/Abilities/Wand/Wand.cs
+++ Assets/Scripts/Abilities/Wand/Wand.cs
@@ -5,22 +5,26 @@ using UnityEngine;
 public class Wand : ActivatableAbility
 {
     // object that will be used as bullet
-    private GameObject projectile;
+    public GameObject projectile;
+    private GameObject player;
     private int damage = 30;
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.Find("Player");
         base.cooldownTimeMax = .2f;
         projectile = (GameObject)Resources.Load("Prefabs/Weapons/Bullet", typeof(GameObject));
     }
 
     public override void Activated()
     {
+
         Vector3 mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 difference = mouse_pos - transform.position;
+        Vector3 difference = mouse_pos - player.transform.position;
         float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
 
-        Bullet bullet = Instantiate(projectile, transform.position, Quaternion.Euler(0f, 0f, rotationZ)).GetComponent<Bullet>();
+        Bullet bullet = Instantiate(projectile, player.transform.position, Quaternion.Euler(0f, 0f, rotationZ)).GetComponent<Bullet>();
+
         bullet.onEnemyHit += bulletHit;
     }
 
--- Assets/Scripts/player/PlayerAbilityManager.cs
+++ Assets/Scripts/player/PlayerAbilityManager.cs
@@ -4,23 +4,95 @@ using UnityEngine;
 
 public class PlayerAbilityManager : MonoBehaviour
 {
-    public ActivatableAbility weapon;
+    // Holds all abilities
+    // Ability 0 or 3: mouse left click
+    // Ability 1 or 4: mouse right click
+    // Ability 2 or 5: space bar
+    private ActivatableAbility[] AbilitySlots = new ActivatableAbility[6];
+
+    // Modifier key (swaps ability slots) shift and tab by default
+    // If true, use abilites 3,4,5 (indexes in the AbilitySlots array)
+    // If false, use abilites 0,1,2
+    public bool useAltAbilities = false;
+
+    bool swapIsOnCooldown = false; // blocks swap so you don't swap a bunch really fast
+    float swapCooldownTime = 1f; // seconds
 
     void Start()
     {
-        weapon = GetComponent<ActivatableAbility>();
+        GameObject player = GameObject.Find("Player");
+        AbilitySlots[0] = player.AddComponent<Wand>() as Wand;
     }
 
+    /// <summary>
+    /// checks for ability keys pressed every update
+    /// </summary>
     void FixedUpdate()
     {
-        DetectFire1();
+        DetectAbilitiesPressed(useAltAbilities);
+        DetectModifierPressed();
     }
 
-    public void DetectFire1()
+    /// <summary>
+    /// Checks if ability keys are pressed and tries to activate them 
+    /// </summary>
+    /// <param name="useAltAbilities">
+    /// If true, uses abilites 3,4,5 (in the AbilitySlots array)
+    /// If false, uses abilites 0,1,2
+    /// </param>
+    public void DetectAbilitiesPressed(bool useAltAbilities)
     {
-        if (Input.GetButton("Fire1"))
+        // this value is added to slot checked 
+        // basically uses slots 0,1,2 by default and 3,4,5 when modified
+        int slotModifierValue = 0;
+        if (useAltAbilities)
         {
-            weapon.Activate();
+            slotModifierValue = 3;
         }
+
+        for (int i = 0; i <= 2; i++)
+        {
+            int abilityNumber = i + slotModifierValue;
+
+            // if ability button is pressed, trigger ability in that slot (if it exists)
+            if (Input.GetButton("Ability" + i))
+            {
+                ActivatableAbility abilityToActivate = AbilitySlots[abilityNumber];
+                if (abilityToActivate != null)
+                {
+                    AbilitySlots[abilityNumber].Activate();
+                }
+
+            }
+        }
+    }
+
+    /// <summary>
+    /// Checks if the modifier key is pressed (default shift or tab) and swaps the
+    /// ability set (123 -> 456) (if this hasn't been done in the last second)
+    /// </summary>
+    public void DetectModifierPressed()
+    {
+        if (Input.GetButton("AbilityModifier"))
+        {
+            if (!swapIsOnCooldown)
+            {
+                swapIsOnCooldown = true;
+                StartCoroutine(ToggleAbilities(swapCooldownTime));
+            }
+        }
+    }
+
+    /// <summary>
+    /// toggles useAltAbilities boolean and waits {seconds} time before allowing
+    /// another toggle
+    /// </summary>
+    /// <param name="seconds"> time to wait (in seconds) </param>
+    /// <returns></returns>
+    private IEnumerator ToggleAbilities(float seconds)
+    {
+        useAltAbilities = !useAltAbilities; // inverse boolean
+        yield return new WaitForSeconds(seconds);
+        swapIsOnCooldown = false; // allow toggling again
     }
 }
