34a99a67fd66d13a6ab432aac3836c17ac2fb37d
Addison <resiget@gmail.com>
2023-06-27 01:20:09 -0500

    added dash method

--- Assets/Scripts/Abilities/Dash/Dash.cs
+++ Assets/Scripts/Abilities/Dash/Dash.cs
@@ -12,21 +12,34 @@ public class Dash : ActivatableAbility
     void Start()
     {
         player = GameObject.Find("Player");
-        base.cooldownTimeMax = 1f;
+        base.cooldownTimeMax = 2f;
     }
 
     public override void Activated()
     {
-        Debug.Log("DASHING");
+        StartCoroutine(Move(player.GetComponent<PlayerMovement>(), player.GetComponent<Rigidbody2D>()));
+    }
 
-        Rigidbody2D rb2d = player.GetComponent<Rigidbody2D>();
-        PlayerMovement movement = player.GetComponent<PlayerMovement>();
-        Vector2 direction = movement.direction;
-
+    /// <summary>
+    /// applies a large force in the direction the player is currently traveling
+    /// gives player back control after dash is complete.
+    /// </summary>
+    /// <param name="movement">player movement script, used to toggle player control</param>
+    /// <param name="rb2d">player physics component</param>
+    /// <returns></returns>
+    private IEnumerator Move(PlayerMovement movement, Rigidbody2D rb2d)
+    {
         movement.isPlayerInControl = false;
 
+        Vector2 direction = movement.direction;
+        rb2d.AddForce(direction * 15, ForceMode2D.Impulse);
+        rb2d.drag = 15;
 
+        while (rb2d.velocity.magnitude > 1)
+        {
+            yield return null;
+        }
 
-        rb2d.velocity = direction * 20;
+        movement.isPlayerInControl = true;
     }
 }
