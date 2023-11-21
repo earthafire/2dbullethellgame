d7b8312ecfa62feb56118677ca275984cb1c9cfe
Addison <resiget@gmail.com>
2023-11-20 20:49:06 -0600

    slime doesn't care if player is dead

--- Assets/Scripts/enemies/Slime.cs
+++ Assets/Scripts/enemies/Slime.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Slime : Enemy
-{    
+{
     private Vector3 target_position;
     public experience xp;
     public GameObject bag;
@@ -24,6 +24,11 @@ public class Slime : Enemy
 
     void SlimeMove()
     {
+        if (player == null)
+        {
+            return;
+        }
+
         Move();
 
         // only change directions when not moving
@@ -36,10 +41,11 @@ public class Slime : Enemy
     override public IEnumerator GetDeath()
     {
         experience _newLoot = Instantiate(xp, transform.position, Quaternion.identity).GetComponent<experience>();
-        if(bag != null){
+        if (bag != null)
+        {
             Instantiate(bag, transform.position, Quaternion.identity);
         }
-        
+
         _newLoot.SetTier(xp_tier);
         yield return base.GetDeath();
     }
