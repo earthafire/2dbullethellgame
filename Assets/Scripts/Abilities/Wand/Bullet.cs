99244d35801fe68dc11970a776d213b4f8f25779
Treefle <zaiahlopossa@gmail.com>
2023-12-09 12:37:42 -0800

    Seeking Bullets
    After a collision bullets can now detect nearby enemies and rotate towards them
    IE: bounce from enemy to enemy

--- Assets/Scripts/Abilities/Wand/Bullet.cs
+++ Assets/Scripts/Abilities/Wand/Bullet.cs
@@ -4,8 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
-{
-
+{ 
     // Event called when hitting an enemy
     public Action<Enemy> onEnemyHit;
 
@@ -15,12 +14,18 @@ public class Bullet : MonoBehaviour
     public float duration = 3f;
     public float speed = 1f;
     public float direction = 0;
+
     private ParticleSystem particles;
     public bool piercingEnabled = true;
-    private int pierceAmount = 3;
+    private int pierceAmount = 10;
+
+    float detectionRange = .5f;
+    Collider2D[] detections;
 
     public void Start()
     {
+        detections = new Collider2D[3];
+
         sound.sfxToPlay.PlaySFX();
         particles = gameObject.GetComponent<ParticleSystem>();
     }
@@ -63,20 +68,20 @@ public class Bullet : MonoBehaviour
             // Calling an event that the ability can subscribe to
             if (enemy.gameObject.layer == 7) // Enemy layer
             {
-                ExplosionParticles();
+                DetectClosestEnemy();
+                //ExplosionParticles();
                 sound.sfxToPlay.PlaySFX();
                 onEnemyHit?.Invoke(enemy);
 
                 if (piercingEnabled)
                 {
                     piercingCount();
+                    RotateTowards(DetectClosestEnemy().transform.position);
                 }
                 else
                 {
                     Destroy(gameObject);
                 }
-                
-                
             }
         }
         catch (System.NullReferenceException)
@@ -93,5 +98,33 @@ public class Bullet : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+    private Enemy DetectClosestEnemy()
+    {
+        float closestDistance = Mathf.Infinity;
+        Enemy closestEnemy = null;
+
+        detections = Physics2D.OverlapCircleAll(transform.position, detectionRange);
+
+        foreach (var col in detections)
+        {
+            if (col.gameObject.layer == 7)
+            {
+                float distance = Vector2.Distance(transform.position, col.transform.position);
+                if (distance < closestDistance)
+                {
+                    closestEnemy = col.gameObject.GetComponent<Enemy>();
+                    closestDistance = distance;
+                }
+            }
+        }
+        return closestEnemy;
+    }
+    private void RotateTowards(Vector3 position)
+    {
+        Vector3 direction = position - transform.position;
+        Vector3 rotatedVectorToTarget = Quaternion.Euler(0, 0, 90) * direction;
+        Quaternion targetRotation = Quaternion.LookRotation(forward: Vector3.forward, upwards: rotatedVectorToTarget);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 45);
     }
 }
