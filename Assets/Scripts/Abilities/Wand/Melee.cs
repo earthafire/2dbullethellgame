349c104eb30e862639245d4e00b74351cfd914da
Addison <resiget@gmail.com>
2024-01-08 16:37:48 -0600

    revert Melee.cs

--- Assets/Scripts/Abilities/Wand/Melee.cs
+++ Assets/Scripts/Abilities/Wand/Melee.cs
@@ -10,7 +10,6 @@ public class Melee : ActivatableAbility
     public int damage = 50;
     public float knockback = 1.5f;
     private Animator animator;
-    private PlayerAttributes playerAttributes;
 
     // Start is called before the first frame update
     void Start()
@@ -18,19 +17,7 @@ public class Melee : ActivatableAbility
         animator = GetComponent<Animator>();
         meleeObj = (GameObject)Resources.Load("Prefabs/Weapons/Wands/Melee", typeof(GameObject));
         player = GameObject.Find("Player");
-        playerAttributes = player.GetComponent<PlayerAttributes>();
-        playerAttributes.attributes.upgradeApplied += UpgradeApplied;
-
-        SetCooldown();
-    }
-    private void OnEnable()
-    {
-      //attributes._playerAttributes.upgradeApplied += UpgradeApplied;
-    }
-
-    private void OnDisable()
-    {
-        playerAttributes.attributes.upgradeApplied -= UpgradeApplied;
+        base.cooldownTimeMax = .5f;
     }
 
     public override void Activated()
@@ -38,17 +25,8 @@ public class Melee : ActivatableAbility
         animator.SetTrigger("Attack");
         MeleeHit meleeHit = Instantiate(meleeObj, player.transform.position, Quaternion.identity).GetComponent<MeleeHit>();
         meleeHit.onEnemyHit += Hit;
-        print (PlayerAttributes.stats[Attribute.cooldown]);
     }
-
-    public void SetCooldown()
-    {
-        base.cooldownTimeMax = PlayerAttributes.stats[Attribute.cooldown];
-    }
-    private void UpgradeApplied(Attributes attribute, UpgradeAttribute upgradeAttribute)
-    {
-        SetCooldown();
-    }
+    
     public void Hit(Enemy enemy)
     {
         enemy.TakeDamage(damage);
